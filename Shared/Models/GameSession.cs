d7ff750497b2d708a056ab7894e216ba76378617
Vuyko Sheshych <vuykosheshych@gmail.com>
2025-02-26 21:00:20 +0200

    fix(models): fixed default GameSession creation rules

--- Shared/Models/GameSession.cs
+++ Shared/Models/GameSession.cs
@@ -1,11 +1,11 @@
 namespace Frontend.Shared.Models;
 public class GameSession
 {
-	public Guid Id { get; set; } = new Guid();
+	public string Id { get; set; } = string.Empty;
 	public string PlayerWhite { get; set; } = string.Empty;
 	public string PlayerBlack { get; set; } = string.Empty;
 	public List<Move> Moves { get; set; } = [];
-	public string CurrentFen { get; set; } = string.Empty;
+	public string CurrentFen { get; set; } = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 	public string Result { get; set; } = "active";
 	public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
