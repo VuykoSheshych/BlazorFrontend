fc38ec75b6d2ba2374debc311aeb4e1fe1cb050d
Vuyko Sheshych <vuykosheshych@gmail.com>
2025-02-26 21:13:50 +0200

    feat(game search): configured game search system
    The server will redirect a selected pair of users who started a game search to an active game

--- Client/Services/GameHubClient.cs
+++ Client/Services/GameHubClient.cs
@@ -1,64 +1,86 @@
 using Microsoft.AspNetCore.SignalR.Client;
 using Frontend.Shared.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace Frontend.Client.Services;
 public class GameHubClient
 {
+	private readonly AuthenticationStateProvider _authenticationStateProvider;
 	private readonly HubConnection _hubConnection;
-
 	public event Action<GameSession>? OnGameStateReceived;
 	public event Action<MoveDto>? OnMoveReceived;
-
-	public GameHubClient(NavigationManager navigationManager)
+	public event Action<string>? OnGameFound;
+	public GameHubClient(NavigationManager navigationManager, AuthenticationStateProvider authenticationStateProvider)
 	{
+		_authenticationStateProvider = authenticationStateProvider;
+
 		_hubConnection = new HubConnectionBuilder()
 			.WithUrl(navigationManager.ToAbsoluteUri("https://localhost:7251/gameHub"))
 			.WithAutomaticReconnect()
 			.Build();
 
+		_hubConnection.StartAsync().ContinueWith(task =>
+		{
+			if (task.IsFaulted)
+			{
+				Console.WriteLine("Error starting connection: " + task.Exception?.Message);
+			}
+			else
+			{
+				Console.WriteLine("Connection started successfully");
+			}
+		});
+
+		_hubConnection.On<string>("GameFound", gameId =>
+		{
+			Console.WriteLine($"Game found: {gameId}");
+			OnGameFound?.Invoke(gameId);
+		});
 		_hubConnection.On<GameSession>("ReceiveGameState", gameSession => OnGameStateReceived?.Invoke(gameSession));
 		_hubConnection.On<MoveDto>("ReceiveMove", move => OnMoveReceived?.Invoke(move));
+		_hubConnection.Closed += async (exception) =>
+		{
+			var user = await GetCurrentUserAsync();
+			await TerminateGameSearch(user);
+		};
 	}
-
-	public async Task ConnectAsync()
+	public async Task Connect()
 	{
 		if (_hubConnection.State == HubConnectionState.Disconnected)
 		{
 			await _hubConnection.StartAsync();
 		}
 	}
-
-	public async Task<string> CreateGameAsync(string player1, string player2)
+	public async Task<string> StartSearch(string player)
 	{
-		if (_hubConnection is not null)
-		{
-			return await _hubConnection.InvokeAsync<string>("CreateGame", player1, player2);
-		}
-
-		throw new InvalidOperationException("SignalR is not connected!");
+		return await _hubConnection.InvokeAsync<string>("StartGameSearch", player);
 	}
-
+	private async Task<string> GetCurrentUserAsync()
+	{
+		var state = await _authenticationStateProvider.GetAuthenticationStateAsync();
+		return state.User.Identity?.Name ?? string.Empty;
+	}
+	public async Task<string> TerminateGameSearch(string player)
+	{
+		return await _hubConnection.InvokeAsync<string>("TerminateGameSearch", player);
+	}
 	public async Task JoinGame(string gameId)
 	{
 		await _hubConnection.InvokeAsync("JoinGame", gameId);
 	}
-
 	public async Task MakeMove(string gameId, MoveDto moveDto)
 	{
 		await _hubConnection.InvokeAsync("MakeMove", gameId, moveDto);
 	}
-
 	public async Task LeaveGame(string gameId)
 	{
 		await _hubConnection.InvokeAsync("LeaveGame", gameId);
 	}
-
 	public async Task FinishGame(string gameId, string result)
 	{
 		await _hubConnection.InvokeAsync("FinishGame", gameId, result);
 	}
-
 	public async Task DisconnectAsync()
 	{
 		await _hubConnection.StopAsync();
