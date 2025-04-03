6d3ee03415c7fe038042b11a5d6f0d5a396913fa
Vuyko Sheshych <vuykosheshych@gmail.com>
2025-04-03 10:28:50 +0300

    feat(logs): add basic loging to Grafana (already not correctly work)

--- Client/Program.cs
+++ Client/Program.cs
@@ -5,10 +5,19 @@ using Frontend.Client;
 using Frontend.Client.Services;
 using MudBlazor.Services;
 using System.Net.Http.Json;
+using Serilog;
+using Serilog.Sinks.Grafana.Loki;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+Log.Logger = new LoggerConfiguration()
+	.WriteTo.GrafanaLoki("http://loki-stack.monitoring.svc.cluster.local:3100")
+	.Enrich.WithProperty("Service", "BlazorWASM")
+	.CreateLogger();
+
+Log.Information("Blazor WASM Client starting...");
 
 string blazorServerUrl;
 string gameplayServiceUrl;
@@ -18,14 +27,20 @@ var config = await httpClient.GetFromJsonAsync<BlazorConfig>("blazor-config.json
 
 if (string.IsNullOrWhiteSpace(config.BLAZOR_SERVER_URL) || string.IsNullOrWhiteSpace(config.GAMEPLAY_URL))
 {
-	throw new Exception($"Invalid config: BLAZOR_SERVER_URL = '{config.BLAZOR_SERVER_URL}', GAMEPLAY_URL = '{config.GAMEPLAY_URL}'");
+	Log.Error("Invalid config: BLAZOR_SERVER_URL = '{BLAZOR_SERVER_URL}', GAMEPLAY_URL = '{GAMEPLAY_URL}'",
+		config.BLAZOR_SERVER_URL, config.GAMEPLAY_URL);
+	throw new Exception("Invalid config values!");
 }
+
+Log.Information("Loaded config: BLAZOR_SERVER_URL = {BLAZOR_SERVER_URL}, GAMEPLAY_URL = {GAMEPLAY_URL}",
+	config.BLAZOR_SERVER_URL, config.GAMEPLAY_URL);
 
 if (builder.HostEnvironment.IsDevelopment())
 {
 	// У випадку запуску проєкту в середовищі розробки використовуються підключення через localhost
 	blazorServerUrl = "https://localhost:7187";
 	gameplayServiceUrl = "https://localhost:7251";
+	Log.Information("Running in Development mode.");
 	// When running the project in a development environment, connections via localhost are used
 }
 else
@@ -33,11 +48,9 @@ else
 	// В інших випадках використовуються змінна середовища
 	blazorServerUrl = config.BLAZOR_SERVER_URL ?? throw new Exception("BLAZOR_SERVER_URL is not set");
 	gameplayServiceUrl = config.GAMEPLAY_URL ?? throw new Exception("GAMEPLAY_URL is not set");
+	Log.Information("Running in Production mode.");
 	// In other cases, environment variables are used
 }
-
-Console.WriteLine($"BLAZOR_SERVER_URL = {blazorServerUrl}");
-Console.WriteLine($"GAMEPLAY_URL = {gameplayServiceUrl}");
 
 builder.Services.AddHttpClient("Frontend.ServerAPI", client => client.BaseAddress = new Uri(blazorServerUrl))
 	.AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
