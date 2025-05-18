5e84af7174fd8adf814738f1aca51761b211e395
VuykoSheshych <kosovan.work@gmail.com>
2025-05-18 16:01:54 +0300

    feat(GameHubClient): change OnGameFound event to async and update handling in GameSearch

--- Features/Game/Services/GameHubClient.cs
+++ Features/Game/Services/GameHubClient.cs
@@ -4,11 +4,12 @@ using ChessShared.Dtos;
 using System.Text.Json;
 
 namespace BlazorFrontend.Features.Game.Services;
+
 public class GameHubClient
 {
 	private readonly HubConnection _hubConnection;
 	public event Func<GameSession, Task>? OnGameStateReceived;
-	public event Action<string>? OnGameFound;
+	public event Func<string, Task>? OnGameFound;
 	public event Action<MoveResultDto>? OnMoveRecieved;
 	public event Func<string, Task>? OnGameFinished;
 	public event Action<ChatMessageDto>? OnMessageReceived;
@@ -21,7 +22,11 @@ public class GameHubClient
 
 		_hubConnection.On<MoveResultDto>("ReceiveMove", moveResult => OnMoveRecieved?.Invoke(moveResult));
 		_hubConnection.On<ChatMessageDto>("ReceiveMessage", chatMessage => OnMessageReceived?.Invoke(chatMessage));
-		_hubConnection.On<string>("GameFound", gameId => OnGameFound?.Invoke(gameId));
+		_hubConnection.On<string>("GameFound", async gameId =>
+		{
+			if (OnGameFound != null)
+				await OnGameFound.Invoke(gameId);
+		});
 		_hubConnection.On<string>("ReceiveGameState", async json =>
 		{
 			var gameSession = JsonSerializer.Deserialize<GameSession>(json);
